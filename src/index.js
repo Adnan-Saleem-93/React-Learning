0efd4fdf027eb922d092aa993c64e0ec746a3e21
Adnan Saleem <adnan13893@gmail.com>
2021-03-31 10:47:24 +0500

    deleted boilerplate code and files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Greetings = () => {
+  return <h2>Greetings User</h2>
+}
+
+export default Greetings
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Greetings />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)

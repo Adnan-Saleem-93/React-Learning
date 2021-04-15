39fab68d171d96ee65a21fa1744c53c121378a20
Adnan Saleem <adnan13893@gmail.com>
2021-04-15 10:02:05 +0500

    added Props

--- src/App.js
+++ src/App.js
@@ -6,34 +6,22 @@ const App = () => {
     // 1. Must return one element in return statement, adjacent elements are not allowed
     // 2. Wrap adjacent elements in a parent element (section, article, div or React Fragment)
     <React.Fragment>
-      <Person /> {/* Nested Component */}
-      <Message />
+      <Person name='John Doe' /> {/* Nested Component */}
+      <Message message='How are you today?' />
     </React.Fragment>
   )
 }
 
-const Person = () => {
-  let name = 'John Doe' // local variable
-  return (
-    <h1 className='person'>
-      {name.toUpperCase()} {/* to use a variable in JSX, wrap it in curly braces */}
-    </h1>
-  )
+const Person = (props) => {
+  /* props is an object that contains properties (key/value pairs)
+  e.g., in this function, props contains the property 'name' whose value is
+  'John Doe' */
+  return <h1 className='person'>{props.name.toUpperCase()}</h1>
 }
 
-const Message = () => {
-  return (
-    <>
-      {/* Code or comments inside HTML must be wrapped in {} (curly braces} */
-      /* inline style attribute is used as an object. */
-      /* html or css properties must be written in camelCase syntax, e.g., fontWeight in style below */}
-      <h3>
-        3+3 = {3 + 3} {/* mathematical evaluations can also be performed in JSX*/}
-        {/* however, we cannot add statements in JSX, as it will result in an error
-         e.g., let x = 6*/}
-      </h3>
-    </>
-  )
+const Message = (props) => {
+  let {message} = props // destructuring the "props" object
+  return <h3>{message}</h3>
 }
 
 export default App

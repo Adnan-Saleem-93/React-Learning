e309ccb063cac447756186bfdd4fb1aa449bb84d
Adnan Saleem <adnan13893@gmail.com>
2021-04-21 11:18:23 +0500

    added children to component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ const App = () => {
     // 1. Must return one element in return statement, adjacent elements are not allowed
     // 2. Wrap adjacent elements in a parent element (section, article, div or React Fragment)
     <React.Fragment>
-      <Person name='John Doe' /> {/* Nested Component */}
-      <Message message='How are you today?' />
+      <Person name="John Doe">
+        <span>Gender: Male</span>
+      </Person>{' '}
+      {/* Nested Component */}
+      <Message message="How are you today?" />
     </React.Fragment>
   )
 }
@@ -16,7 +19,13 @@ const Person = (props) => {
   /* props is an object that contains properties (key/value pairs)
   e.g., in this function, props contains the property 'name' whose value is
   'John Doe' */
-  return <h1 className='person'>{props.name.toUpperCase()}</h1>
+  return (
+    <>
+      <h1 className="person">{props.name.toUpperCase()}</h1>
+      {/* elements inside components can be extracted using "children" */}
+      <h2>{props.children}</h2>
+    </>
+  )
 }
 
 const Message = (props) => {

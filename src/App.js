a6f176fe1ef793acbe6165a1f0b181d9289492bb
Adnan Saleem <adnan13893@gmail.com>
2021-04-26 16:14:46 +0500

    useState basics

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.css'
 
 const App = () => {
-  let people = [
-    {id: 1, name: 'John Doe'},
-    {id: 2, name: 'Jon Snow'},
-    {id: 3, name: 'Big Show'}
-  ]
-
-  // EVENTS Example
-  // Event handlers can be used in 3 ways in React:
-  // 1. as inline functions
-  // 2. by referencing a functions name
-  // 3. if a function requires parameters, we use inline functions to reference a function in pass parameters along
-
   return (
     <React.Fragment>
-      <Person {...people[0]} />
+      <Person />
     </React.Fragment>
   )
 }
 
 const Person = ({name}) => {
-  const onNameClick = () => {
-    alert('clicked')
-  }
-  const onNameClickWithParams = (name) => {
-    alert('Hello ' + name)
-  }
+  let [count, setCount] = useState(0) // array destructuring
+
   return (
-    <>
-      <h1 className="person" onClick={() => alert(name)}>
-        {/* 1. inline function to handle event */}
-        1. {name.toUpperCase()}
-      </h1>
-      <h1 className="person" onClick={onNameClick}>
-        {/* 2. reference another function to handle event */}
-        2. {name.toUpperCase()}
-      </h1>
-      <h1 className="person" onClick={() => onNameClickWithParams(name)}>
-        {/* 3. use inline function to reference another function and pass arguments */}
-        3. {name.toUpperCase()}
-      </h1>
-    </>
+    <div className="counter">
+      <h1 className="person">{count}</h1>
+      <button type="button" style={{color: 'blue'}} onClick={() => setCount(count + 1)}>
+        <b>+</b>
+      </button>
+      <button type="button" style={{color: 'green'}} onClick={() => setCount(0)}>
+        <b>RESET</b>
+      </button>
+      <button type="button" style={{color: 'red'}} onClick={() => setCount(count - 1)}>
+        <b>-</b>
+      </button>
+    </div>
   )
 }
 

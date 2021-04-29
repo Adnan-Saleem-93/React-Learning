9dd857790959637ebfd20f67057148f637e0ce0b
Adnan Saleem <adnan13893@gmail.com>
2021-04-29 12:11:38 +0500

    useState - Objects

--- src/App.js
+++ src/App.js
@@ -12,30 +12,32 @@ const App = () => {
 }
 
 const Person = ({name}) => {
-  let [people, setPeople] = useState(data) // array destructuring
-  let [newPerson, setNewPerson] = useState('')
+  let [state, setState] = useState({people: data, newPerson: ''}) // array destructuring
+  let {people, newPerson} = state
 
   // function to add a new item
   const onAddNew = () => {
     let newPeople = [...people]
     newPeople.push({id: people[people.length - 1].id + 1, name: newPerson}) // pusing to item to the new array
-    setPeople(newPeople)
+    setState({...state, people: newPeople})
   }
 
   // function to clear all items
   const onClear = () => {
-    setPeople([])
+    setState({...state, people: []})
   }
 
   // function to remove a specific item
   const onRemoveItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id) // filtering array
-    setPeople(newPeople)
+    setState({...state, people: newPeople})
   }
 
   // function to update the newPerson state
   const onChange = (event) => {
-    setNewPerson(event.target.value)
+    let newObject = {...state}
+    newObject.newPerson = event.target.value
+    setState(newObject)
   }
 
   return (

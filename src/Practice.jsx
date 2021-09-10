49b056e611ba468efbb02a9120ccdab433beefdc
Adnan Saleem <adnan13893@gmail.com>
2021-09-10 12:47:46 +0500

    useRef hook

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,94 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "./index.css";
 
 const Practice = () => {
-  const [user, setUser] = useState({name: "", email: "", age: 0});
-  const [userList, setUserList] = useState([]);
+  const [value, setValue] = useState("");
+  const counter = useRef(0);
+  const textInput = useRef(null);
 
-  const handleChange = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-    setUser({...user, [name]: value});
+  useEffect(() => {
+    // Every time the component has been re-rendered,
+    // the counter is incremented
+    counter.current = counter.current + 1;
+  });
+  const handleClick = () => {
+    textInput.current.value = "selected";
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (user.name && user.email && user.age) {
-      setUserList((prevUsers) => {
-        let newUsers = [...prevUsers];
-        newUsers.push({id: newUsers.length + 1, name: user.name, email: user.email, age: user.age});
-        return [...newUsers];
-      });
-      setUser({name: "", email: "", age: ""});
-    } else {
-      alert("All fields are required.!");
-    }
-  };
-
+  console.log("render " + parseInt(counter.current + 1));
   return (
-    <div className="main">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <label htmlFor="name" className="col-md-3">
-            User Name : &ensp;
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-control col-md-7"
-            autoComplete="true"
-            autoFocus={true}
-            value={user.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="row">
-          <label htmlFor="email" className="col-md-3">
-            Email : &ensp;
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="form-control col-md-7"
-            autoComplete="true"
-            value={user.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="row">
-          <label htmlFor="age" className="col-md-3">
-            Age : &ensp;
-          </label>
-          <input
-            type="number"
-            min="0"
-            id="age"
-            name="age"
-            className="form-control col-md-7"
-            autoComplete="false"
-            value={user.age}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-      {userList.length > 0 && (
-        <>
-          {userList.map((item, index) => {
-            return (
-              <div key={index} className="user">
-                <b>{item.name}</b>
-                <span>{item.age}</span>
-                <span>{item.email}</span>
-              </div>
-            );
-          })}
-        </>
-      )}
+    <div className="container-sm mx-auto mt-5 text-center">
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="form-control"
+        ref={textInput}
+      />
+      <h1>{`The component has been re-rendered ${counter.current} times`}</h1>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Click
+      </button>
     </div>
   );
 };

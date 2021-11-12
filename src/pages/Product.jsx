417305232080b78341230c6498e4de3b2c0e6fc4
Adnan Saleem <adnan13893@gmail.com>
2021-11-12 09:24:25 +0500

    removed unused imports, moved files to appropriate location

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import defaultImage from './images/unnamed.png'
+import defaultImage from '../images/unnamed.png'
 import {Link} from 'react-router-dom'
 
 const Product = ({id, name, price, image}) => {
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import {useFetch} from '../useFetch'
-import Product from '../Product'
+import Product from './Product'
 import {useGlobalContext} from '../useGlobalContext'
 
 const Products = () => {
